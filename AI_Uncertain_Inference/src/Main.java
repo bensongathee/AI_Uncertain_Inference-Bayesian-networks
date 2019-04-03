372f639cd3273daa1712f4971ae06e527e38c0cd
bensongathee <benson7484@gmail.com>
2019-04-02 21:35:43 -0400

    updated file

--- AI_Uncertain_Inference/src/Main.java
+++ AI_Uncertain_Inference/src/Main.java
@@ -15,24 +15,24 @@ import bn.parser.BIFParser;
 import bn.parser.XMLBIFParser;
 public class Main {
 	public static void main(String[] argv) throws IOException, ParserConfigurationException, SAXException {
-		String filename = null;
 		String networkName = null;
+		String filename = null;
 		
 		// Creating a Bayesian Network
 		BayesianNetwork bn = null;
 		if(argv.length > 0 && argv[0].equals("bif")) {
 			if (argv.length > 1) {
-				filename = argv[1];
-				if (argv.length > 2)
-					networkName = argv[2];
+				networkName = argv[1];
+				if (argv.length > 3)
+					filename = argv[3];
 			}
 			BIFParser parser = new BIFParser(new FileInputStream(filename));
 			bn = parser.parseNetwork();
 		}else if(argv.length > 0 && argv[0].equals("xml")){
 			if (argv.length > 1) {
-				filename = argv[1];
-				if (argv.length > 2)
-					networkName = argv[2];
+				networkName = argv[1];
+				if (argv.length > 3)
+					filename = argv[3];
 			}
 			XMLBIFParser parser = new XMLBIFParser();
 			bn = parser.readNetworkFromFile(filename);
@@ -40,12 +40,12 @@ public class Main {
 		
 		// Creating the Query Variable
 		RandomVariable X = null;
-		if(argv[3] != null)
-			X = bn.getVariableByName(argv[3]);
+		if(argv[4] != null)
+			X = bn.getVariableByName(argv[4]);
 		
 		// Creating the a list of observed values of the Evidence Variables
 		Assignment a = new bn.base.Assignment();
-		int count= 3;
+		int count= 4;
 		while(argv.length>++count)
 			a.put(bn.getVariableByName(argv[count]), new StringValue(argv[++count]));
 		
@@ -53,23 +53,31 @@ public class Main {
 		// Passing the 3 Arguments onto the inferencer to obtain the distribution
 		Inferencer exact = new EnumerationInferencer();
 		Distribution dist1 = exact.query(X, a, bn);
-		System.out.print(networkName+" distribution:\tP("+argv[3]+"|");
-		for(int i = 4; i < argv.length; i+=2)
+		System.out.print("Inference by enumeration:\n\t"+networkName+": Distribution over "+argv[4]+" = \tP("+argv[4]+"|");
+		for(int i = 5; i < argv.length; i+=2)
 			System.out.print(" "+argv[i] + " = "+argv[i+1]);
-		System.out.print(") = "+dist1+"\n");
+		System.out.print(") = "+dist1+"\n\n");
 		
-		Inferencer rej = new RejectionSamplingInferencer();
-		Distribution dist2 = rej.query(X, a, bn);
-		System.out.print(networkName+" distribution:\tP("+argv[3]+"|");
-		for(int i = 4; i < argv.length; i+=2)
+		RejectionSamplingInferencer rej = new RejectionSamplingInferencer();
+		Distribution dist2 = null;
+		if(argv[2].equals("-"))
+			dist2 = rej.query(X, a, bn);
+		else 
+			dist2 = rej.query(X, a, bn, Integer.parseInt(argv[2]));
+		System.out.print("Rejection Sampling:\n\t"+networkName+": Distribution over "+argv[4]+" = \tP("+argv[4]+"|");
+		for(int i = 5; i < argv.length; i+=2)
 			System.out.print(" "+argv[i] + " = "+argv[i+1]);
-		System.out.print(") = "+dist2+"\n");
+		System.out.print(") = "+dist2+"\n\n");
 		
-		Inferencer lik = new LikelihoodWeightingInferencer();
-		Distribution dist3 = lik.query(X, a, bn);
-		System.out.print(networkName+" distribution:\tP("+argv[3]+"|");
-		for(int i = 4; i < argv.length; i+=2)
+		LikelihoodWeightingInferencer lik = new LikelihoodWeightingInferencer();
+		Distribution dist3 = null;
+		if(argv[2].equals("-"))
+			dist3 = lik.query(X, a, bn);
+		else 
+			dist3 = lik.query(X, a, bn, Integer.parseInt(argv[2]));
+		System.out.print("Rejection Sampling:\n\t"+networkName+": Distribution over "+argv[4]+" = \tP("+argv[4]+"|");
+		for(int i = 5; i < argv.length; i+=2)
 			System.out.print(" "+argv[i] + " = "+argv[i+1]);
-		System.out.print(") = "+dist3+"\n");
+		System.out.print(") = "+dist3+"\n\n");
 	}
 }
